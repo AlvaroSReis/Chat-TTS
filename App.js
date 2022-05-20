23bcbb90e20b61420b8de13824924d466d8a8cfd
AlvaroSR <alvaro.reis@dcomp.ufs.br>
2022-05-20 05:59:06 -0300

    app migration

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-screens';
+import 'react-native-gesture-handler'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Login from './screens/login.js'
+import Chat from './screens/chat.js'
+import Profile from './screens/profile.js'
+import Cadastro from './screens/cadastro.js'
+
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+
+export default function App (Component) {
+
+  //render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login"  component={Login}/>
+          <Stack.Screen name="Cadastro" component={Cadastro} />
+          <Stack.Screen name="Home" component={Home}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+    
+    function Home() {
+      return (
+          <Drawer.Navigator>
+            <Drawer.Screen name ="Chat" component={Chat} />
+            <Drawer.Screen name ="Profile" component={Profile} />
+          </Drawer.Navigator>
+      )
+    }
+  //}
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    position: 'relative'
+  },
+
+  bottomView: {
+    flexDirection: "row",
+    padding: 5,
+    marginBottom: 20
+  },
+
+  txtInput: {
+    flex: 3,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    padding: 10,
+    borderRadius: 10
+  },
+
+  btn: {
+    flex: 1,
+    borderRadius: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#204cfa'
+  },
+
+  btnTxt: {
+    color: '#fff'
+  },
+
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
+
